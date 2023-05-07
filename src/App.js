4db056f43d93d00d63fe43bc3fbc36ffea451bee
Vishesh Pandey <74998585+Vishesh-Pandey@users.noreply.github.com>
2023-05-08 03:27:14 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,96 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [method, setMethod] = useState("GET")
+  const [url, setUrl] = useState("")
+  const [body, setBody] = useState("")
+  const [response, setResponse] = useState("")
+
+  const sendRequest = async () => {
+    try {
+      const res = await fetch(url, {
+        method
+
+      })
+      const data = await res.json()
+      console.log(data)
+      setResponse(JSON.stringify(data, null, 2))
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav className="navbar bg-body-tertiary">
+        <div className="container-fluid">
+          <span className="navbar-brand mb-0 h1">v-api</span>
+        </div>
+      </nav>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-2">
+            <select value={method} onChange={(e) => setMethod(e.target.value)} className="form-select" >
+
+              <option value="GET">GET</option>
+              <option value="POST">POST</option>
+              <option value="PUT">PUT</option>
+              <option value="DELETE">DELETE</option>
+            </select>
+
+          </div>
+          <div className="col-10">
+            <div className="input-group mb-3">
+              <input
+                value={url}
+                onChange={(e) => setUrl(e.target.value)}
+                type="text"
+                className="form-control"
+                placeholder="Enter URL"
+
+              />
+              <button
+                onClick={sendRequest}
+                className="btn btn-outline-secondary"
+                type="button"
+                id="button-addon2"
+              >
+                Send
+              </button>
+            </div>
+
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <div className="form-floating">
+              <textarea
+                value={body}
+                onChange={(e) => setBody(e.target.value)}
+                className="form-control"
+                placeholder="Leave a comment here"
+                id="floatingTextarea2"
+                style={{ height: 100 }}
+
+              />
+              <label htmlFor="floatingTextarea2">Body</label>
+            </div>
+
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            {response}
+          </div>
+        </div>
+      </div>
+
+    </>
   );
 }
 
